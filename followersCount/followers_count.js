64dea53e6787b60983e8aaf36c48dfa787ff1573
debomalya <aakash.deb08@gmail.com>
2025-04-06 20:29:56 +0530

    follower's count implemented using functions

---
+++ followersCount/followers_count.js
@@ -0,0 +1,19 @@
+let count =0 ;
+
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML =count
+}
+
+function checkCountValue(){
+    if (count ===10){
+        alert("Your Instagram post gained 10 followers! Congrats")
+    } else if (count ===20){
+        alert("Your instagram post gained 20 followers! Keep it going")
+    }
+}
+
+function increaseCount(){
+    count ++ ;
+    displayCount();
+    checkCountValue();
+}
