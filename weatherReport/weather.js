ca3e5ddc4412341088d7d5374a61e85bd3c7ea51
debomalya <aakash.deb08@gmail.com>
2025-04-24 11:55:23 +0530

    added

---
+++ weatherReport/weather.js
@@ -0,0 +1,17 @@
+function showweatherDetails(event) {
+    event.preventDefault();
+    const city = document.getElementById('city').value;
+const apiKey = 'b05a6f78b46884349e20e76a16f00934'; // Replace 'YOUR_API_KEY' with your actual API key
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+fetch(apiUrl)
+.then(response => response.json())
+.then(data => {
+  const weatherInfo = document.getElementById('weatherInfo');
+  weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                          <p>Temperature: ${data.main.temp} &#8451;</p>
+                          <p>Weather: ${data.weather[0].description}</p>`;
+})
+
+}
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
